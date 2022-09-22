61b4457655953acd702041e0f98bacd4b7751bb4
yezi <yehaowei20060411@qq.com>
2022-09-22 10:15:33 +0800

    chore: 添加部分基础模块

---
+++ utils/db.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+
+from utils.config import config
+
+
+def init_DB(db_name: str):
+    connection: MongoClient = MongoClient(config.db.host, config.db.port)
+    db = connection[db_name]
+    return db
+
+
+db = init_DB(config.db.main_database)
+
+
+def get_collection(collection_name: str):
+    return db[collection_name]
+
+
+trade_data_db = db.trade_data
+auth_data_db = db.auth_data
